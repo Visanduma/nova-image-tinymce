ee6631d963bf1b2b51939f6b3883f0368c49f727
LaHiRu <lahirulhr@gmail.com>
2022-05-25 13:23:45 +0530

    naming fixes

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,7 +1,7 @@
 Nova.booting((Vue, router, store) => {
-  Vue.component('index-nova-tinymce5-editor', require('./components/IndexField'))
-  Vue.component('detail-nova-tinymce5-editor', require('./components/DetailField'))
-  Vue.component('form-nova-tinymce5-editor', require('./components/FormField'))
+  Vue.component('index-nova-image-tinymce-editor', require('./components/IndexField'))
+  Vue.component('detail-nova-image-tinymce-editor', require('./components/DetailField'))
+  Vue.component('form-nova-image-tinymce-editor', require('./components/FormField'))
   Vue.component('media-picker-nova-tinymce5-editor', require('./components/addons/media/ImagePicker'))
   Vue.component('image-block-mt', require('./components/addons/media/ImageBlock'))
   Vue.config.devtools = true
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('get-images',[ config('nova-tinymce5-editor.options.media_handler'),'getImages']);
-Route::get('search-images/{query}',[ config('nova-tinymce5-editor.options.media_handler'),'searchImages']);
-Route::post('upload-image',[ config('nova-tinymce5-editor.options.media_handler'),'uploadImage']);
-Route::post('delete-images',[ config('nova-tinymce5-editor.options.media_handler'),'deleteImages']);
+Route::get('get-images',[ config('nova-image-tinymce.media_handler'),'getImages']);
+Route::get('search-images/{query}',[ config('nova-image-tinymce.media_handler'),'searchImages']);
+Route::post('upload-image',[ config('nova-image-tinymce.media_handler'),'uploadImage']);
+Route::post('delete-images',[ config('nova-image-tinymce.media_handler'),'deleteImages']);
